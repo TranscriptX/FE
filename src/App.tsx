62f95ba2a0971974561aeebb4299dbb72741bdae
xTorch8 <esantosa.es@gmail.com>
2025-05-13 16:50:07 +0700

    F6-Sprint2-LandingPage - Add Landing Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,42 @@
 import { AuthProvider } from "./context/AuthContext";
-import './App.css'
-import './index.css'
+import "./App.css";
+import "./index.css";
 import { BrowserRouter, Route, Routes } from "react-router";
 import RegisterPage from "./pages/authentication/RegisterPage";
 import LoginPage from "./pages/authentication/LoginPage";
 import ForgotPage from "./pages/authentication/ForgotPage";
-import Tools from './pages/Menu/Tools';
-import DocumentSummarizer from './pages/Menu/DocumentSummarizer';  
-import AudioVideoTranscription from './pages/Menu/AudioVideoTranscription';  
-import Dashboard from './pages/Dashboard/Dashboard';  
-import ViewWorkspace from './pages/Dashboard/ViewWorkspace';  
-import EditWorkspace from './pages/Dashboard/EditWorkspace';  
-import ExportWorkspace from './pages/Dashboard/ExportWorkspace';  
-import ResetPage from './pages/authentication/ResetPage';
+import Tools from "./pages/Menu/Tools";
+import DocumentSummarizer from "./pages/Menu/DocumentSummarizer";
+import AudioVideoTranscription from "./pages/Menu/AudioVideoTranscription";
+import Dashboard from "./pages/Dashboard/Dashboard";
+import ViewWorkspace from "./pages/Dashboard/ViewWorkspace";
+import EditWorkspace from "./pages/Dashboard/EditWorkspace";
+import ExportWorkspace from "./pages/Dashboard/ExportWorkspace";
+import ResetPage from "./pages/authentication/ResetPage";
+import LandingPage from "./pages/landing/LandingPage";
 
 function App() {
-  return ( 
-    <AuthProvider>
-        <BrowserRouter>
-            <Routes>
-                <Route path="login" element={<LoginPage/>} />
-                <Route path="register" element={<RegisterPage/>} />
-                <Route path="forgot" element={<ForgotPage/>} />
-                <Route path="/" element={<RegisterPage/>} />
-                <Route path="/Tools" element={<Tools />} />
-                <Route path="/document-summarizer" element={<DocumentSummarizer />} />
-                <Route path="/audio-video-transcription" element={<AudioVideoTranscription />} />
-                <Route path="/Dashboard" element={<Dashboard />} />
-                <Route path="/view-workspace/:id" element={<ViewWorkspace />} />
-                <Route path="/edit-workspace/:id" element={<EditWorkspace />} />
-                <Route path="/ExportWorkspace" element={<ExportWorkspace />} />
-                <Route path="/reset-password" element={<ResetPage />} />
-                {/* <Route path="profile" element={<ProfilePage/>} /> */}
-            </Routes>
-        </BrowserRouter>
-    </AuthProvider>
-  );
+	return (
+		<AuthProvider>
+			<BrowserRouter>
+				<Routes>
+					<Route path="login" element={<LoginPage />} />
+					<Route path="register" element={<RegisterPage />} />
+					<Route path="forgot" element={<ForgotPage />} />
+					<Route path="/" element={<LandingPage />} />
+					<Route path="/Tools" element={<Tools />} />
+					<Route path="/document-summarizer" element={<DocumentSummarizer />} />
+					<Route path="/audio-video-transcription" element={<AudioVideoTranscription />} />
+					<Route path="/Dashboard" element={<Dashboard />} />
+					<Route path="/view-workspace/:id" element={<ViewWorkspace />} />
+					<Route path="/edit-workspace/:id" element={<EditWorkspace />} />
+					<Route path="/ExportWorkspace" element={<ExportWorkspace />} />
+					<Route path="/reset-password" element={<ResetPage />} />
+					{/* <Route path="profile" element={<ProfilePage/>} /> */}
+				</Routes>
+			</BrowserRouter>
+		</AuthProvider>
+	);
 }
 
 export default App;
